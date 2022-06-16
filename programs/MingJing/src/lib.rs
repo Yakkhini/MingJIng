5e1d17ad762e5401fac500c599e4e9e5eb4666b5
Yakkhini <59007159+Yakkhini@users.noreply.github.com>
2022-06-17 01:39:08 +0800

    Feat: Jing Zhan account.

--- programs/MingJing/src/lib.rs
+++ programs/MingJing/src/lib.rs
@@ -12,6 +12,13 @@ See the Mulan PSL v2 for more details.
 
 use anchor_lang::prelude::*;
 
+/// constents
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4;
+const MAX_CONTENT_LENGTH: usize = 280 * 4;
+
 declare_id!("5q1C1Xk4d45nEsNFR19ZXwjXFKZgKkSpkDy1yH8hQwFZ");
 
 #[program]
@@ -25,3 +32,18 @@ pub mod ming_jing {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+/// Jing Zhan is basic post on Ming Jing, like Tweet on Twitter or Weibo.
+#[account]
+pub struct JingZhan {
+    pub author: Pubkey,
+    pub timestamp: i64,
+    pub content: String,
+}
+
+impl JingZhan {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH // Author.
+        + TIMESTAMP_LENGTH // Timestamp
+        + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH; // Content.
+}
