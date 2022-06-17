9851ab01e67b2bc0e3fadbf0730cb730c1210cfe
Yakkhini <59007159+Yakkhini@users.noreply.github.com>
2022-06-18 01:54:17 +0800

    Test: More test, content length check and other user post check.

--- tests/MingJing.ts
+++ tests/MingJing.ts
@@ -12,6 +12,7 @@ See the Mulan PSL v2 for more details.
 
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { assert } from "chai";
 import { MingJing } from "../target/types/ming_jing";
 
 describe("MingJing", () => {
@@ -21,15 +22,73 @@ describe("MingJing", () => {
   const program = anchor.workspace.MingJing as Program<MingJing>;
 
   it("can post a new jin zhan", async () => {
-    const jinzhan = anchor.web3.Keypair.generate();
+    const jinZhan = anchor.web3.Keypair.generate();
     await program.methods
       .postJinzhan("Do not bother me.")
       .accounts({
-        jinzhan: jinzhan.publicKey,
+        jinzhan: jinZhan.publicKey,
         author: anchor.AnchorProvider.env().wallet.publicKey,
         systemProgram: anchor.web3.SystemProgram.programId,
       })
-      .signers([jinzhan])
+      .signers([jinZhan])
       .rpc();
+  });
+
+  it("can post a new jin zhan with another author.", async () => {
+    const otherUser = anchor.web3.Keypair.generate();
+    const signature = await program.provider.connection.requestAirdrop(
+      otherUser.publicKey,
+      1000000000
+    );
+    await program.provider.connection.confirmTransaction(signature);
+    const jinZhan = anchor.web3.Keypair.generate();
+    await program.methods
+      .postJinzhan("PLZ just stay by my side I'm really upset.")
+      .accounts({
+        jinzhan: jinZhan.publicKey,
+        author: otherUser.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([otherUser, jinZhan])
+      .rpc();
+    const jinZhanAccount = await program.account.jinZhan.fetch(
+      jinZhan.publicKey
+    );
+
+    assert.equal(
+      jinZhanAccount.author.toBase58(),
+      otherUser.publicKey.toBase58()
+    );
+    assert.equal(
+      jinZhanAccount.content,
+      "PLZ just stay by my side I'm really upset."
+    );
+    assert.ok(jinZhanAccount.timestamp);
+  });
+
+  it("cannot provide a Jin Zhan with more than 280 characters", async () => {
+    try {
+      const jinZhan = anchor.web3.Keypair.generate();
+      const contentWith281Chars = "v".repeat(281);
+      await program.methods
+        .postJinzhan(contentWith281Chars)
+        .accounts({
+          jinzhan: jinZhan.publicKey,
+          author: anchor.AnchorProvider.env().wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .signers([jinZhan])
+        .rpc();
+    } catch (error) {
+      assert.equal(
+        error.error.errorMessage,
+        "This jin zhan has too much characters."
+      );
+      return;
+    }
+
+    assert.fail(
+      "Post instruction would fail with a 281-character content Jin Zhan"
+    );
   });
 });
