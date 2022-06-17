21a493dae17fe940d9b1bade62f0e557e318efb7
Yakkhini <59007159+Yakkhini@users.noreply.github.com>
2022-06-18 01:05:37 +0800

    Test: post jin zhan test.

--- README.md
+++ README.md
@@ -5,6 +5,12 @@ A Dapp for Ming Jing community.
 
 **明镜** 是以重开研究指导的一个社区解决方案，采用重开逻辑为基础的交流方式。用户在交流时可以对彼此的意见提出质询，构成 意象 - 推断 - 驳斥的方式，弥合人与人的割裂，更有效地统合不同人的意见。
 
+## 技术选型 Tools
+
+本项目是一个智能合约程序，位于 Solana 公链上。相较于 Ethereum，Solana 的性能更好，Gas Fee 更低廉。项目的数据库存储在 Solana 公链上，但受制于比较高昂的 Storage Rent，所以在开发测试初期每一条进展被设计为只能携带 280 字符的内容。但在未来会考虑使用 Arweave 存储技术，以更低廉的价格提供更多的存储空间，使得图片、视频上传成为可能。
+
+本项目使用了 Anchor 框架开发 Solana Program。
+
 ## 参考 Reference
 
 本项目参考 [solana-twitter](https://lorisleiva.com/create-a-solana-dapp-from-scratch) 编写。
--- tests/MingJing.ts
+++ tests/MingJing.ts
@@ -1,3 +1,15 @@
+/*
+Copyright (c) 2022 Yakkhini
+MingJing is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+         http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+*/
+
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { MingJing } from "../target/types/ming_jing";
@@ -8,9 +20,16 @@ describe("MingJing", () => {
 
   const program = anchor.workspace.MingJing as Program<MingJing>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("can post a new jin zhan", async () => {
+    const jinzhan = anchor.web3.Keypair.generate();
+    await program.methods
+      .postJinzhan("Do not bother me.")
+      .accounts({
+        jinzhan: jinzhan.publicKey,
+        author: anchor.AnchorProvider.env().wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([jinzhan])
+      .rpc();
   });
 });
